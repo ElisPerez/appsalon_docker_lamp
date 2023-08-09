4ab0d953acc55136643f66e5ee1b2d796a20ee7c
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-08-08 23:24:31 -0500

    Todo listo funcionando para comenzar a hacer codigo

--- sql/02_createTable.sql
+++ sql/02_createTable.sql
@@ -5,6 +5,18 @@
           PRIMARY KEY (id)
           );
 
-SHOW TABLES;
-
 DESCRIBE services;
+
+   CREATE TABLE IF NOT EXISTS appointments (
+          id INT (11) NOT NULL AUTO_INCREMENT,
+          firstname VARCHAR(60) NOT NULL,
+          lastname VARCHAR(60) NOT NULL,
+          appointment_hour TIME DEFAULT NULL,
+          appointment_date DATE DEFAULT NULL,
+          services VARCHAR(255) NOT NULL,
+          PRIMARY KEY (id)
+          );
+
+DESCRIBE appointments;
+
+SHOW TABLES;
